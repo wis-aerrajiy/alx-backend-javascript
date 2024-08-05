ddb556015d7f5f1eb5af3fc55be2a72b108989cf
wis-aerrajiy <frabce25@gmail.com>
2024-08-05 15:00:57 +0100

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const index of array) {
-    newArray.push(array[index] + appendString);
+    newArray.push(`${appendString}${index}`);
   }
 
   return newArray;
