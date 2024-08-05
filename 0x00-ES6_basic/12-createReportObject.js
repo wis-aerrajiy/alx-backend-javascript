fa351c6e51377036b7b3c07a12ecd3a39ce8cb70
wis-aerrajiy <frabce25@gmail.com>
2024-08-05 15:11:34 +0100

    12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   const report = {
-    'allEmployees': employeesList,
-    getEmployeeCount: () => employeesList.length,
+    allEmployees: employeesList,
+    getEmployeeCount: () => Object.keys(employeesList).length,
   };
 
   return report;
