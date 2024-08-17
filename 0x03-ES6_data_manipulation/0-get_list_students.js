1db01dc5f6d036e8a1bfefc3ecb446f959eb3bba
wis-aerrajiy <frabce25@gmail.com>
2024-08-17 01:29:59 +0100

    b

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,18 @@
 const getListStudents = () => [
   {
     id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco",
+    firstName: 'Guillaume',
+    location: 'San Francisco',
   },
   {
     id: 2,
-    firstName: "James",
-    location: "Columbia",
+    firstName: 'James',
+    location: 'Columbia',
   },
   {
     id: 5,
-    firstName: "Serena",
-    location: "San Francisco",
+    firstName: 'Serena',
+    location: 'San Francisco',
   },
 ];
 
